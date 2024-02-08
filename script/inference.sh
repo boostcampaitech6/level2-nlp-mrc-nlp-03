1a765d8b5df30b7fb6373cfc70b33429fabb6d76
jingi-data <imfeelo@gmail.com>
2024-02-08 14:54:24 +0900

    chore: overwrite as default
    * 학습과 추론 결과가 덮어쓰이도록 설정

--- script/inference.sh
+++ script/inference.sh
@@ -3,4 +3,5 @@ python3 code/inference.py \
     --output_dir code/outputs/test_dataset/ \
     --dataset_name data/test_dataset/ \
     --model_name_or_path code/models/train_dataset/ \
+    --overwrite_output_dir \
     --do_predict
--- script/train.sh
+++ script/train.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/sh
 python3 code/train.py \
-    --model_name_or_path monologg/koelectra-small-v3-finetuned-korquad \
+    --model_name_or_path monologg/koelectra-base-v3-finetuned-korquad \
     --output_dir code/models/train_dataset \
+    --overwrite_cache \
+    --overwrite_output_dir \
     --do_train \
     --do_eval
