9bd381bd03436200cb86b6c49ac4bb22cd20ecca
gyunini <gyunini@gmail.com>
2024-02-20 15:17:15 +0900

    feat: roberta-large 사용 bm25 tokenizer 교체

--- code/arguments.py
+++ code/arguments.py
@@ -8,7 +8,6 @@ class ModelArguments:
     """Arguments pertaining to which model/config/tokenizer we are going to fine-tune from."""
 
     model_name_or_path: str = field(
-        # default="klue/bert-base",
         default="klue/roberta-large",
         metadata={
             "help": "Path to pretrained model or model identifier from huggingface.co/models"
